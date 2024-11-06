bc6a1fd90df6c58fc64ee7122ec50b2d89448829
Rita-Centeno <20211579@novaims.unl.pt>
2024-11-06 16:35:14 +0000

    Improving argparser and paths

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,30 +1,18 @@
+import os
 import argparse
 import pandas as pd
 
-def load_data(path: str) -> pd.DataFrame:
-    '''
-    Loads the data from a given path.
-    
-    Args:
-        path (str): The path to the data file.
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(SCRIPT_DIR, 'data')
 
-    Returns:
-        pd.DataFrame: The loaded data.
-    '''
+def load_data(path: str) -> pd.DataFrame:
+    '''Loads the data from a given path'''
+
     return pd.read_csv(path, sep='\t')
 
 def clean_data(data_to_clean: pd.DataFrame, country: str = 'PT') -> pd.DataFrame:
-    '''
-    Cleans the data. Unpivots the data, removes NaNs, and filters only the data 
-    where region equals to PT (Portugal).
-    
-    Args:
-        data_to_clean (pd.DataFrame): The data to be cleaned.
-        country (str): The country code to filter the data by. Default is 'PT'.
-    
-    Returns:
-        pd.DataFrame: The cleaned data.
-    '''
+    '''Cleans the data. Unpivots the data, removes NaNs, and filters only the data 
+    where region equals to PT (Portugal)'''
 
     # Unpivot the data
     split_columns = data_to_clean['unit,sex,age,geo\\time'].str.split(',', expand=True)
@@ -48,28 +36,19 @@ def clean_data(data_to_clean: pd.DataFrame, country: str = 'PT') -> pd.DataFrame
     return cleaned_data[cleaned_data['region'] == country]
 
 def save_data(data_to_save: pd.DataFrame, path: str) -> None:
-    '''
-    Saves data to the given data folder.
-    
-    Args:
-        data_to_save (pd.DataFrame): The data to be saved.
-        output_path (str): The path to save the data to.
-    '''
+    '''Saves data to the given data folder'''
 
     return data_to_save.to_csv(path, index=False)
 
-def main(filter_country: str,
-         input_path: str = 'life_expectancy/data/eu_life_expectancy_raw.tsv',
-         output_path: str = 'life_expectancy/data/pt_life_expectancy.csv') -> None:
-    '''
-    Main function to load, clean and save the data.
+def main(filter_country: str = 'PT') -> None:
+    '''Main function to load, clean and save the data'''
 
-    Args:
-        filter_country (str): The country code to filter the data by.
-        input_path (str): The path to the data file.
-        output_file (str): The path to save the cleaned data.
-    '''
+    # Define the data paths
+    input_path = os.path.join(DATA_PATH, 'eu_life_expectancy_raw.tsv')
+    output_file_name = f'{filter_country.lower()}_life_expectancy.csv'
+    output_path = os.path.join(DATA_PATH, output_file_name)
 
+    # Load, clean and save the data
     loaded_data = load_data(path=input_path)
     cleaned_data = clean_data(data_to_clean=loaded_data, country=filter_country)
     save_data(data_to_save=cleaned_data, path=output_path)
@@ -77,11 +56,5 @@ def main(filter_country: str,
 if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser()
     parser.add_argument('--country', type=str, default='PT', help='Country code (default: PT)')
-    parser.add_argument('--input_path', type=str,
-            default='life_expectancy/data/eu_life_expectancy_raw.tsv',
-            help='Path to the data file')
-    parser.add_argument('--output_path', type=str,
-            default='life_expectancy/data/pt_life_expectancy.csv',
-            help='Path to save the cleaned data')
     args = parser.parse_args()
-    main(args.country, args.input_path, args.output_path)
+    main(args.country)
