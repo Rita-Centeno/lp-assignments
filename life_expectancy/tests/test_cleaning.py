b7b73ade960739525c05b8b5e36a6cf1abbc1e80
Rita-Centeno <20211579@novaims.unl.pt>
2024-11-06 21:08:46 +0000

    added tests to load_data and save_data functions

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,7 +1,13 @@
 """Tests for the cleaning module"""
+import os
+import sys
+from unittest.mock import patch
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+# Add the parent directory (life_expectancy) to sys.path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from life_expectancy.cleaning import clean_data # pylint: disable=C0413
+from life_expectancy.loading_saving import load_data, save_data # pylint: disable=C0413
 
 
 def test_clean_data(eu_life_expectancy_raw_sample, eu_life_expectancy_raw_sample_expected):
@@ -16,3 +22,21 @@ def test_clean_data(eu_life_expectancy_raw_sample, eu_life_expectancy_raw_sample
     pd.testing.assert_frame_equal(
         cleaned_data, output_data_fix
     )
+
+def test_load_data(eu_life_expectancy_raw_sample):
+    """Test the load_data function"""
+
+    with patch('pandas.read_csv', return_value=eu_life_expectancy_raw_sample):
+        data = load_data("the_data_path.csv")
+        assert data.equals(eu_life_expectancy_raw_sample)
+
+def test_save_data():
+    """Test the save_data function"""
+
+    with patch('pandas.DataFrame.to_csv') as mock_to_csv:
+        df = pd.DataFrame({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6]
+        })
+        save_data(df, "the_data_path.csv")
+        mock_to_csv.assert_called_once_with("the_data_path.csv", index=False)
