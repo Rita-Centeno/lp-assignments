fdffc30969790ab15f0596743274fe2667d3a570
Rita-Centeno <20211579@novaims.unl.pt>
2024-11-06 19:56:25 +0000

    modify test_cleaning to take the fixtures

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,16 +1,18 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import main
-from . import OUTPUT_DIR
+from life_expectancy.cleaning import clean_data
 
 
-def test_clean_data(pt_life_expectancy_expected):
+def test_clean_data(eu_life_expectancy_raw_sample, eu_life_expectancy_raw_sample_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    main(filter_country="PT")
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+
+    input_data_fix = eu_life_expectancy_raw_sample
+    output_data_fix = eu_life_expectancy_raw_sample_expected
+
+    cleaned_data = clean_data(input_data_fix)
+    cleaned_data.reset_index(drop=True, inplace=True)
+
     pd.testing.assert_frame_equal(
-        pt_life_expectancy_actual, pt_life_expectancy_expected
+        cleaned_data, output_data_fix
     )
